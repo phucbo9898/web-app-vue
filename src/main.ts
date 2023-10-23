5d0a7d30f42242ccb04c8b1722632089bafe4b2f
phucbo9898 <86301310+phucbo9898@users.noreply.github.com>
2023-10-23 21:12:28 +0700

    init

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './assets/scss/index.scss'
+import { BootstrapVue } from 'bootstrap-vue'
+import { getModule } from 'vuex-module-decorators'
+import { Component } from 'vue-property-decorator'
+// @ts-ignore
+// import Vidle from 'v-idle'
+
+Vue.use(BootstrapVue)
+// Vue.use(Vidle)
 
 Vue.config.productionTip = false
 
