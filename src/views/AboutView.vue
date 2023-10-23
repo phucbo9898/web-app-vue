aa49d5c4552b2a9774e68f6bdc9c6a2db8586df5
phucvn <phucvn@newwave.com>
2023-10-23 10:00:51 +0700

    example module: save data to local_storage

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,15 +3,27 @@
     <h1>This is an about page</h1>
     <input type="text" name="testInput" v-model="testInput">
     <span>{{ testInput }}</span>
-    <h1>test</h1>
-    <h2>test02</h2>
+    <button @click="submitInput" class="btn btn-secondary">Click</button>
+    <button @click="resetInput" class="btn btn-secondary">Reset</button>
   </div>
 </template>
 
 <script lang="ts">
+import store from '@/store';
+import Auth from '@/store/modules/Auth';
 import { Component, Vue } from 'vue-property-decorator';
+import { getModule } from 'vuex-module-decorators';
+const AuthModule = getModule(Auth, store)
 @Component
 export default class AboutVie extends Vue{
   private testInput: any = ''
+
+  submitInput() {
+    AuthModule.SET_NEW_TOKEN(this.testInput)
+  }
+
+  resetInput() {
+    AuthModule.setTokenNull()
+  }
 }
 </script>
