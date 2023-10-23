8069540817b9bdf22b4bfe96093836c5829b19d5
phucvn <phucvn@newwave.com>
2023-10-23 09:26:06 +0700

    Fix bug v-model not working

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <input type="text" name="testInput" v-model="testInput">
+    <span>{{ testInput }}</span>
+    <h1>test</h1>
+    <h2>test02</h2>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+@Component
+export default class AboutVie extends Vue{
+  private testInput: any = ''
+}
+</script>
